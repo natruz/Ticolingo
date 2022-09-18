93a7d7ae43e578addd7e7c419ffe7829b0344518
Tay Kai Quan <kaitay303@gmail.com>
2022-09-18 09:46:54 +0800

    Add persistence (not enabled in previews)

--- Ticolingo/Vocab/Data/StudyGroups.swift
+++ Ticolingo/Vocab/Data/StudyGroups.swift
@@ -8,15 +8,52 @@
 import Foundation
 
 class StudyGroups: ObservableObject {
-    // TODO: Read from memory
-    static let shared: StudyGroups = .init(studyGroups: [])
+    static let shared: StudyGroups = .init()
 
     @Published
-    var studyGroups: [StudySetGroup]
+    var studyGroups: [StudySetGroup] {
+        didSet {
+            if saveToMemory {
+                save()
+            }
+        }
+    }
 
-    init(studyGroups: [StudySetGroup]) {
-        self.studyGroups = studyGroups
-        self.studyGroups.append(contentsOf: StudyGroups.textBookSets)
-        self.studyGroups.append(contentsOf: StudyGroups.otherData)
+    var saveToMemory: Bool = false
+
+    /// Creates a ``StudyGroups`` instance. If `studyGroups` is given, the `StudyGroups` is created with the studyGroups as the initial value.
+    /// If `includeDefaults` is `false`, the default items are not included. If `studyGroups` is not given, the code will attempt to read from memory,
+    /// ignoring the value of `includeDefaults`.
+    ///
+    /// - Parameters:
+    ///   - studyGroups: The initial value of the StudyGroups. If not provided, the last saved studyGroups are read from memory.
+    ///   - includeDefaults: If the StudyGroups should include the default items. Ignored if studyGroups is not provided.
+    init(studyGroups: [StudySetGroup]? = nil, includeDefaults: Bool = true) {
+        self.studyGroups = []
+
+        // If is being run in previews, don't bother with loading since it will just use up memory.
+        // Previews should only be used for rapid UI or small scale UX development, so persistence is not needed.
+        if ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1" {
+            let studyGroups = studyGroups ?? []
+            self.studyGroups = includeDefaults ? addBasicGroups(studyGroups: studyGroups) : studyGroups
+            return
+        }
+
+        // If studygroups were specified, do not load and use those instead.
+        if let studyGroups = studyGroups {
+            self.studyGroups = addBasicGroups(studyGroups: studyGroups)
+        } else {
+            // If no study groups were specified, try to load from memory.
+            // In this case, includeDefaults is ignored.
+            load()
+            self.saveToMemory = true
+        }
+    }
+
+    func addBasicGroups(studyGroups: [StudySetGroup] = []) -> [StudySetGroup] {
+        var mutableStudyGroups = studyGroups
+        mutableStudyGroups.append(contentsOf: StudyGroups.textBookSets)
+        mutableStudyGroups.append(contentsOf: StudyGroups.otherData)
+        return mutableStudyGroups
     }
 }
---
+++ Ticolingo/Vocab/Data/StudyGroupsManager.swift
@@ -0,0 +1,42 @@
+//
+//  StudyGroupsManager.swift
+//  Ticolingo
+//
+//  Created by TAY KAI QUAN on 18/9/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension StudyGroups {
+
+    func getArchiveURL() -> URL {
+        let plistName = "StudyGroupss.plist"
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+
+        return documentsDirectory.appendingPathComponent(plistName)
+    }
+
+    func save() {
+        let archiveURL = getArchiveURL()
+        let propertyListEncoder = PropertyListEncoder()
+        let encodedStudyGroupsItems = try? propertyListEncoder.encode(studyGroups)
+        try? encodedStudyGroupsItems?.write(to: archiveURL, options: .noFileProtection)
+    }
+
+    func load() {
+        let archiveURL = getArchiveURL()
+        let propertyListDecoder = PropertyListDecoder()
+
+        var finalStudyGroupsItems: [StudySetGroup]!
+
+        if let retrievedStudyGroupsItemData = try? Data(contentsOf: archiveURL),
+            let decodedStudyGroupsItems = try? propertyListDecoder.decode([StudySetGroup].self, from: retrievedStudyGroupsItemData) {
+                finalStudyGroupsItems = decodedStudyGroupsItems
+        } else {
+            finalStudyGroupsItems = addBasicGroups()
+        }
+
+        studyGroups = finalStudyGroupsItems
+    }
+}
