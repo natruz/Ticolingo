fba2a504e7ec3efdf30d8d560faae59817df8dbf
Tay Kai Quan <kaitay303@gmail.com>
2022-09-18 09:21:32 +0800

    Stop the ">" Icon from showing up

--- Ticolingo/SetHierarchy/TermDetailView.swift
+++ Ticolingo/SetHierarchy/TermDetailView.swift
@@ -12,80 +12,86 @@ struct TermDetailView: View {
     @State var term: Vocab
     
     var body: some View {
-        VStack {
-            List {
-                Section {
-                    VStack(alignment: .leading) {
-                        Text("Pinyin: ")
-                            .foregroundColor(.gray)
-                            .padding(.bottom, 1)
-                        Text(term.pinyin)
-                    }
-                    VStack(alignment: .leading) {
-                        Text("Definition: ")
-                            .foregroundColor(.gray)
-                            .padding(.bottom, 1)
-                        Text(term.definition)
-                    }
-                    VStack(alignment: .leading) {
-                        Text("Example Sentence: ")
-                            .foregroundColor(.gray)
-                            .padding(.bottom, 1)
-                        ForEach(Array(term.exampleSentences.enumerated()), id: \.offset.self) { index, exampleSentence in
-                            Text("\(index + 1). \(exampleSentence)")
-                        }
-                    }
-                    VStack(alignment: .leading) {
-                        Text("Difficulty: \(term.difficulty)/7")
-                            .foregroundColor(.gray)
-                            .padding(.bottom, 1)
-                        HStack {
-                            ForEach(0..<term.difficulty, id: \.self ) { _ in
-                                Image(systemName: "star.fill")
-                                    .foregroundColor(.yellow)
-                            }
-                            ForEach(term.difficulty..<7, id: \.self) { _ in
-                                Image(systemName: "star.fill")
-                                    .foregroundColor(.gray)
-                            }
-                        }
+        List {
+            Section {
+                VStack(alignment: .leading) {
+                    Text("Pinyin: ")
+                        .foregroundColor(.gray)
+                        .padding(.bottom, 1)
+                    Text(term.pinyin)
+                }
+                VStack(alignment: .leading) {
+                    Text("Definition: ")
+                        .foregroundColor(.gray)
+                        .padding(.bottom, 1)
+                    Text(term.definition)
+                }
+                VStack(alignment: .leading) {
+                    Text("Example Sentence: ")
+                        .foregroundColor(.gray)
+                        .padding(.bottom, 1)
+                    ForEach(Array(term.exampleSentences.enumerated()), id: \.offset.self) { index, exampleSentence in
+                        Text("\(index + 1). \(exampleSentence)")
                     }
                 }
-                Section {
-                    VStack(alignment: .leading) {
-                        Text("How familiar are you with this word?")
-                            .padding(.bottom, 1)
-                    }
+                VStack(alignment: .leading) {
+                    Text("Difficulty: \(term.difficulty)/7")
+                        .foregroundColor(.gray)
+                        .padding(.bottom, 1)
                     HStack {
-                        Spacer()
-                        Button {
-                            print("sad")
-                            term.familiarity = false
-                        } label: {
-                           Text("☹️")
-                                .font(.title)
-                                .padding()
-                                .background(.red)
-                                .cornerRadius(15)
+                        ForEach(0..<term.difficulty, id: \.self ) { _ in
+                            Image(systemName: "star.fill")
+                                .foregroundColor(.yellow)
                         }
-                        .buttonStyle(PlainButtonStyle())
-                        Spacer()
-                        Button {
-                            print("happy")
-                            term.familiarity = true
-                        } label: {
-                           Text("😃")
-                                .font(.title)
-                                .padding()
-                                .background(.green)
-                                .cornerRadius(15)
+                        ForEach(term.difficulty..<7, id: \.self) { _ in
+                            Image(systemName: "star.fill")
+                                .foregroundColor(.gray)
                         }
-                        .buttonStyle(PlainButtonStyle())
-                        Spacer()
                     }
                 }
             }
-            .navigationTitle(term.term)
+            Section {
+                VStack(alignment: .leading) {
+                    Text("How familiar are you with this word?")
+                        .padding(.bottom, 1)
+                }
+                HStack {
+                    Spacer()
+                    Button {
+                        print("sad")
+                        term.familiarity = false
+                    } label: {
+                        Text("☹️")
+                            .font(.title)
+                            .padding()
+                            .background(.red)
+                            .cornerRadius(15)
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    Spacer()
+                    Button {
+                        print("happy")
+                        term.familiarity = true
+                    } label: {
+                        Text("😃")
+                            .font(.title)
+                            .padding()
+                            .background(.green)
+                            .cornerRadius(15)
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    Spacer()
+                }
+            }
+        }
+        .navigationTitle(term.term)
+        .toolbar {
+            ToolbarItem(id: "edit", placement: .navigationBarTrailing) {
+                Button("Edit") {
+
+                }
+                .disabled(!term.editable)
+            }
         }
     }
 }
