46b2db75ae1d7403a0b0e0f34eae6cbb4435633a
Tay Kai Quan <kaitay303@gmail.com>
2022-10-22 10:20:04 +0800

    Add screen to connect playing the game to the StudySetView

---
+++ Ticolingo/Quizzes/QuizSelectTypeView.swift
@@ -0,0 +1,123 @@
+//
+//  QuizSelectTypeView.swift
+//  Ticolingo
+//
+//  Created by TAY KAI QUAN on 22/10/22.
+//
+
+import SwiftUI
+
+struct QuizSelectTypeView: View {
+
+    @State var vocab: [Vocab]
+    @Binding var questions: [Question]
+
+    @State var questionFormat: String = "Character"
+    @State var answerFormat: String = "PinYin"
+
+    let qnaFormatOptions = [
+        "Character",
+        "PinYin",
+        "Definition"
+    ]
+
+    var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                HStack(alignment: .center) {
+                    Spacer()
+                    VStack {
+                        Text("Question")
+                        Picker("Question Format", selection: $questionFormat, content: {
+                            ForEach(qnaFormatOptions, id: \.self) { format in
+                                Text(format)
+                            }
+                        })
+                        .pickerStyle(.menu)
+                    }
+                    .onChange(of: questionFormat) { _ in
+                        if questionFormat == answerFormat {
+                            let questionIndex = qnaFormatOptions.firstIndex(of: questionFormat)!
+                            let newIndex = (questionIndex+1) % qnaFormatOptions.count
+                            answerFormat = qnaFormatOptions[newIndex]
+                        }
+                        changeQuestions()
+                    }
+                    .frame(width: (geometry.size.width-70)/2)
+
+                    Spacer()
+                    Image(systemName: "arrow.forward")
+                        .font(.system(size: 50))
+                        .frame(width: 50)
+                    Spacer()
+
+                    VStack {
+                        Text("Answer")
+                        Picker("Question Format", selection: $answerFormat, content: {
+                            ForEach(qnaFormatOptions, id: \.self) { format in
+                                Text(format)
+                            }
+                        })
+                        .pickerStyle(.menu)
+                    }
+                    .onChange(of: answerFormat) { _ in
+                        if questionFormat == answerFormat {
+                            let questionIndex = qnaFormatOptions.firstIndex(of: answerFormat)!
+                            let newIndex = (questionIndex-1) % qnaFormatOptions.count
+                            questionFormat = qnaFormatOptions[newIndex]
+                        }
+                        changeQuestions()
+                    }
+                    .frame(width: (geometry.size.width-50)/2)
+                    Spacer()
+                }
+            }
+            .frame(height: geometry.size.height)
+        }
+        .onAppear {
+            changeQuestions()
+        }
+    }
+
+    func changeQuestions() {
+        guard questionFormat != answerFormat else { return }
+
+        questions = []
+        questions = vocab.map { vocab in
+            var question = "unknown question"
+            var answer = "unknown answer"
+
+            switch questionFormat {
+            case "Character":
+                question = vocab.term
+            case "PinYin":
+                question = vocab.pinyin
+            case "Definition":
+                question = vocab.definition
+            default: break
+            }
+
+            switch answerFormat {
+            case "Character":
+                answer = vocab.term
+            case "PinYin":
+                answer = vocab.pinyin
+            case "Definition":
+                answer = vocab.definition
+            default: break
+            }
+
+            return Question(question: question, answer: answer)
+        }
+    }
+}
+
+struct QuizSelectTypeView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuizSelectTypeView(vocab: [
+            Vocab(term: "高兴", definition: "happy", exampleSentence: "我很高兴认识你。", difficulty: 0),
+            Vocab(term: "快乐", definition: "happiness", exampleSentence: "小学的时候我很快乐。", difficulty: 0),
+        ],
+                           questions: .constant([]))
+    }
+}
--- Ticolingo/SetHierarchy/StudySetView.swift
+++ Ticolingo/SetHierarchy/StudySetView.swift
@@ -10,25 +10,48 @@ import SwiftUI
 struct StudySetView: View {
     
     @State var set: StudySet
+    @State var questions: [Question] = []
     
     var body: some View {
         List {
-            ForEach(set.terms) { term in
-                NavigationLink(destination: TermDetailView(term: term)) {
-                    Text(term.familiarity ? "😃" : "☹️")
-                    Text(term.term)
-                    Text(term.pinyin)
-                        .opacity(0.8)
+            Section {
+                NavigationLink {
+                    QuizSelectTypeView(vocab: set.terms,
+                                       questions: $questions)
+                    NavigationLink {
+                        DragAndMatchView(options: questions)
+                    } label: {
+                        ZStack(alignment: .center) {
+                            Color.accentColor
+                                .frame(width: 120, height: 40)
+                                .cornerRadius(10)
+                            Text("Start Playing")
+                                .foregroundColor(Color(uiColor: UIColor.label.inverted))
+                        }
+                    }
+                } label: {
+                    Text("Drag and Match Quiz")
                 }
-                .deleteDisabled(!set.editable)
-                .moveDisabled(!set.editable)
             }
-            .onMove(perform: { index, moveTo in
-                print("Tried to move \(index) to \(moveTo)")
-            })
-            .onDelete(perform: { index in
-                print("Tried to delete \(index)")
-            })
+            Section {
+                ForEach(set.terms) { term in
+                    NavigationLink(destination: TermDetailView(term: term)) {
+                        Text(term.familiarity ? "😃" : "☹️")
+                        Text(term.term)
+                        Text(term.pinyin)
+                            .opacity(0.8)
+                    }
+                    .deleteDisabled(!set.editable)
+                    .moveDisabled(!set.editable)
+                }
+                .onMove(perform: { index, moveTo in
+                    print("Tried to move \(index) to \(moveTo)")
+                })
+                .onDelete(perform: { index in
+
+                    print("Tried to delete \(index)")
+                })
+            }
         }
         .navigationTitle(set.title)
     }
@@ -42,5 +65,28 @@ struct StudySetView_Previews: PreviewProvider {
                 Vocab(term: "快乐", definition: "happiness", exampleSentence: "小学的时候我很快乐。", difficulty: 0),
             ]))
         }
+    }
+}
+
+extension UIColor {
+    var inverted: UIColor {
+        var alpha: CGFloat = 1.0
+
+        var red: CGFloat = 0.0, green: CGFloat = 0.0, blue: CGFloat = 0.0
+        if self.getRed(&red, green: &green, blue: &blue, alpha: &alpha) {
+            return UIColor(red: 1.0 - red, green: 1.0 - green, blue: 1.0 - blue, alpha: alpha)
+        }
+
+        var hue: CGFloat = 0.0, saturation: CGFloat = 0.0, brightness: CGFloat = 0.0
+        if self.getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha) {
+            return UIColor(hue: 1.0 - hue, saturation: 1.0 - saturation, brightness: 1.0 - brightness, alpha: alpha)
+        }
+
+        var white: CGFloat = 0.0
+        if self.getWhite(&white, alpha: &alpha) {
+            return UIColor(white: 1.0 - white, alpha: alpha)
+        }
+
+        return self
     }
 }
