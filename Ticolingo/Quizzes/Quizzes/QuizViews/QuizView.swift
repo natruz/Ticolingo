0d6dbee13e7e7b0299ceb5a08f37da5d4dc2840c
Tay Kai Quan <kaitay303@gmail.com>
2022-11-02 20:27:38 +0800

    Make quiz view work with traditional `Question` type (incomplete)

--- Ticolingo/Quizzes/Quizzes/QuizViews/QuizView.swift
+++ Ticolingo/Quizzes/Quizzes/QuizViews/QuizView.swift
@@ -8,114 +8,88 @@
 import SwiftUI
 
 struct QuizView: View {
+    @State var options: [(Question, [String])]
     @State var questionIndex = 0
-    @State var correctans = false
-    @State var wrongans = false
-    @State var qnCorrect = 0
-    @State var isSheet = false
-    
-    var body: some View {
-        ZStack {
-            VStack {
-                ProgressView(value: Double(questionIndex + 1), total: Double(Quiz.count))
-                    .padding()
-                
-                Text("\(Quiz[questionIndex].qn)")
-                    .font(.title)
-                    .padding()
-                    .foregroundColor(.black)
-                Spacer()
-                HStack {
-                    VStack {
-                        Text("\(Quiz[questionIndex].word)")
-                            .padding()
-                            .font(.system(size: 100))
-                        Spacer()
-                        Button {
-                            if Quiz[questionIndex].answer == 0 {
-                                correctans = true
-                                qnCorrect += 1
-                                questionIndex += 1
-                            }
-                            else {
-                                wrongans = true
-                                questionIndex += 1
-                            }
-                        } label: {
-                            Text("▲ \(Quiz[questionIndex].options[0])")
-                                .frame(width: 280, height: 10)
-                                .font(.system(size: 17))
-                                .padding()
-                                .background(Color.red)
-                                .foregroundColor(.white)
-                                .cornerRadius(10)
-                        }
-                        Button {
-                            if Quiz[questionIndex].answer == 0 {
-                                correctans = true
-                                qnCorrect += 1
-                                questionIndex += 1
-                            }
-                            else {
-                                wrongans = true
-                                questionIndex += 1
-                            }
-                        } label: {
-                            Text("■ \(Quiz[questionIndex].options[1])")
-                                .frame(width: 280, height: 10)
-                                .font(.system(size: 17))
-                                .padding()
-                                .background(Color.yellow)
-                                .foregroundColor(.white)
-                                .cornerRadius(10)
-                        }
-                        Button {
-                            if Quiz[questionIndex].answer == 0 {
-                                correctans = true
-                                qnCorrect += 1
-                                questionIndex += 1
-                            }
-                            else {
-                                wrongans = true
-                                questionIndex += 1
-                            }
-                        } label: {
-                            Text("◆ \(Quiz[questionIndex].options[2])")
-                                .frame(width: 280, height: 10)
-                                .font(.system(size: 17))
-                                .padding()
-                                .background(Color.green)
-                                .foregroundColor(.white)
-                                .cornerRadius(10)
-                        }
-                        Button {
-                            if Quiz[questionIndex].answer == 0 {
-                                correctans = true
-                                qnCorrect += 1
-                                questionIndex += 1
-                            }
-                            else {
-                                wrongans = true
-                                questionIndex += 1
-                            }
-                        } label: {
-                            Text("● \(Quiz[questionIndex].options[3])")
-                                .frame(width: 280, height: 10)
-                                .font(.system(size: 17))
-                                .padding()
-                                .background(Color.blue)
-                                .foregroundColor(.white)
-                                .cornerRadius(10)
-                        }
-                    }
-                }
-            }
+    @State var correctQuestions = [Question]()
+    @State var wrongQuestions = [Question]()
+
+    init(options: [Question]) {
+        let allAnswers = options.map { $0.answer }
+
+        self.options = options.map { option in
+            // remove the correct answer from the possible random answers
+            // shuffle the possible other answers
+            let possibleOtherAnswers = allAnswers.filter({ $0 != option.answer }).shuffled()
+
+            // get the first 3 (or fewer) possible other answers
+            // if we asked for exactly 3 other answers, the app would crash if there
+            // were fewer than 4 questions, so we need this min function to avoid that.
+            var answers = possibleOtherAnswers[0..<min(3, possibleOtherAnswers.count)]
+
+            // add the correct answer, shuffle, and then return
+            answers.append(option.answer)
+            return (option , answers.shuffled())
         }
     }
     
-    struct QuizView_Previews: PreviewProvider {
-        static var previews: some View {
-            QuizView()
+    var body: some View {
+        ZStack {
+            GeometryReader { geometry in VStack {
+                // progress indicator
+                ProgressView(value: Double(questionIndex), total: Double(options.count))
+                    .scaleEffect(x: 1, y: 4, anchor: .center)
+                    .padding()
+
+                // question
+                // this view reuses the single flip card view for its auto
+                // resizing text. It does not flip.
+                SingleFlipCardView(front: $options[questionIndex].0.question,
+                                   back: .constant(""),
+                                   frontColor: .clear,
+                                   onFlip: { _ in .reject })
+                    .frame(height: geometry.size.height/2)
+
+                // answer options
+                VStack {
+                    ForEach(0..<options[questionIndex].1.count, id: \.self) { index in
+                        buttonForOption(optionNo: index)
+                    }
+                } // no need to set the frame here since it'll automatically fill the
+                  // rest of the view
+            }}
         }
+    }
+
+    @ViewBuilder
+    func buttonForOption(optionNo: Int) -> some View {
+        Button {
+            if options[questionIndex].0.answer == options[questionIndex].1[optionNo] { // correct
+                correctQuestions.append(options[questionIndex].0)
+                questionIndex += 1
+            } else { // wrong
+                wrongQuestions.append(options[questionIndex].0)
+                questionIndex += 1
+            }
+        } label: {
+            Text("\(options[questionIndex].1[optionNo])")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(buttonColors[optionNo])
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .padding(.horizontal, 10)
+        }
+    }
+
+    // this arrangement theoretically allows up to 6 options
+    let buttonColors: [Color] = [.red, .yellow, .green, .blue, .orange]
+}
+
+struct QuizView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuizView(options: [Question(question: "a", answer: "1"),
+                           Question(question: "b", answer: "2"),
+                           Question(question: "c", answer: "3"),
+                           Question(question: "d", answer: "4"),
+                           Question(question: "e", answer: "5")])
     }
 }
