42ba7c33e48ad70ec97e6ae1f78bce6bfae7b351
Tay Kai Quan <kaitay303@gmail.com>
2022-11-03 22:44:51 +0800

    Implement QuizProtocolView to standardise quizzes

---
+++ Ticolingo/Quizzes/Quizzes/QuizProtocolView.swift
@@ -0,0 +1,134 @@
+//
+//  QuizProtocolView.swift
+//  Ticolingo
+//
+//  Created by TAY KAI QUAN on 3/11/22.
+//
+
+import SwiftUI
+
+protocol QuizProtocolView: View {
+    /// The total number of questions in the quiz. MUST BE MARKED `@State`
+    var total: Int { get set }
+    /// The total number of completed questions in the quiz. MUST BE MARKED `@State`
+    var completed: Int { get set }
+    /// The total number of wrong questions in the quiz. MUST BE MARKED `@State`
+    var wrong: Int? { get set }
+    /// The total number of correct questions in the quiz. MUST BE MARKED `@State`
+    var correct: Int? { get set }
+
+    var statsToShow: [Stat] { get set }
+}
+
+enum Stat: CaseIterable {
+    /// The number of questions in the quiz
+    case total
+    /// The number of questions completed in the quiz
+    case completed
+    /// The remaining questions
+    case remaining
+    /// How many wrong answers were given
+    case wrong
+    /// How many correct answers were given
+    case correct
+
+    /// The colors for each stat, used in the stats view of ``QuizProtocolView``
+    static let colors: [Stat: Color] = [
+        .total: Color.orange,
+        .completed: Color.indigo,
+        .remaining: Color.cyan,
+        .wrong: Color.red,
+        .correct: Color.green
+    ]
+
+    /// The color for the `Stat`, which redirects to the ``colors`` static property of `Stat`.
+    var color: Color {
+        Stat.colors[self] ?? Color.clear
+    }
+}
+
+extension QuizProtocolView {
+    @ViewBuilder
+    /// A stat view, which provides statistics according to the ``statsToShow`` variable.
+    /// Does not wrap the stats in a `HStack`, `VStack`, etc. so it is up to the developer
+    /// to decide what to wrap it with, when appropriate.
+    var stats: some View {
+        ForEach(statsToShow, id: \.self) { stat in
+            ZStack {
+                stat.color
+                    .frame(height: 50)
+                    .cornerRadius(10)
+                    .opacity(0.5)
+                HStack {
+                    switch stat {
+                    case .total:
+                        Text("Total")
+                            .padding(.bottom, 0)
+                        Text("\(total)")
+                            .font(.system(size: 30))
+                    case .completed:
+                        Text("Done")
+                            .padding(.bottom, 0)
+                        Text("\(completed)")
+                            .font(.system(size: 30))
+                    case .remaining:
+                        Text("Left")
+                            .padding(.bottom, 0)
+                        Text("\(total-completed)")
+                            .font(.system(size: 30))
+                    case .wrong:
+                        Text("Wrong")
+                            .padding(.bottom, 0)
+                        Text("\(wrong ?? 0)")
+                            .font(.system(size: 30))
+                    case .correct:
+                        Text("Right")
+                            .padding(.bottom, 0)
+                        Text("\(correct ?? 0)")
+                            .font(.system(size: 30))
+                    }
+                }
+            }
+        }
+    }
+}
+
+private struct TestQuizView: QuizProtocolView {
+    @State var total: Int = 0
+    @State var completed: Int = 0
+    @State var wrong: Int? = 0
+    @State var correct: Int? = 0
+    @State var statsToShow: [Stat] = [
+        .total,
+        .completed,
+        .remaining,
+        .wrong,
+        .correct
+    ]
+
+    var body: some View {
+        VStack {
+            stats
+            HStack {
+                VStack {
+                    Button("Increase total") { total += 1 }
+                    Button("Increase completed") { completed += 1 }
+                    Button("Increase wrong") { wrong = (wrong ?? 0) + 1 }
+                    Button("Increase right") { correct = (correct ?? 0) + 1 }
+                }
+                VStack {
+                    Button("Decrease total") { total -= 1 }
+                    Button("Decrease completed") { completed -= 1 }
+                    Button("Decrease wrong") { wrong = (wrong ?? 0) - 1 }
+                    Button("Decrease right") { correct = (correct ?? 0) - 1 }
+                }
+            }
+        }
+    }
+}
+
+struct QuizProtocolView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestQuizView()
+    }
+}
