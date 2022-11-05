e72dd53162752ba0b48efe4b5481032a89237cf3
Tay Kai Quan <kaitay303@gmail.com>
2022-11-05 10:35:33 +0800

    Make DragAndMatchView conform to QuizProtocolView

--- Ticolingo/Quizzes/Quizzes/DragAndMatchView.swift
+++ Ticolingo/Quizzes/Quizzes/DragAndMatchView.swift
@@ -7,88 +7,57 @@
 
 import SwiftUI
 
-struct DragAndMatchView: View {
+struct DragAndMatchView: QuizProtocolView {
 
-    let options: [Question]
-    @State var scores: [Question: Double?] = [:]
+    @State var total: Int
+    @State var completed: Int
+    @State var wrong: Int? = 0
+    @State var correct: Int? = nil
+    @State var questions: [Question]
+    @State var randomised: Bool
+    @State var statsToShow: [Stat] = [.remaining, .completed, .wrong]
+    @State var attempts: [Question : (Int, Int)]
     
     let cardSize: CGFloat = 40.0
-    let topBarSize: CGFloat = 50.0
 
-    init(options: [Question]) {
-        self.options = options
-        let empty: [Double?] = options.map({ _ in nil })
-        self.scores = Dictionary(uniqueKeysWithValues: zip(options, empty))
+    init(options: [Question], randomised: Bool = false) {
+        self._total = State(initialValue: options.count)
+        self._completed = State(initialValue: 0)
+        self._questions = State(initialValue: randomised ? options.shuffled() : options)
+        self._randomised = State(initialValue: randomised)
+        self._attempts = State(initialValue: [:])
     }
 
-    @State var leftOptions: [String] = []
-    @State var rightOptions: [String] = []
+    @State var currentOptions: [Question] = []
+    @State var leftArrangement: [Int] = [0, 1, 2, 3, 4]
+    @State var rightArrangement: [Int] = [0, 1, 2, 3, 4].shuffled()
 
     @State var translation: CGSize = .zero
-    @State var draggedCard: Int = -1
-    @State var pairedIndexes: [Int] = []
-
-    @State var wrongAnswers: Int = 0
+    @State var draggedCard: Question?
+    @State var pairedQuestions: [Question] = []
 
     var body: some View {
         ZStack {
-            if pairedIndexes.count == options.existingCount {
-                VStack {
-                    stats
-                }
-                .frame(width: 200)
-                VStack {
-                    Button("Restart") {
-                        withAnimation {
-                            restart()
-                        }
-                    }
-                    .padding(.bottom, 20)
-                    NavigationLink("Finish") {
-                        // TODO: Reimplement QuizResultsView
-//                        QuizResultsView(scores: scores)
-                    }
-                }
-                .offset(y: 200)
-            } else {
+            if completed < total {
                 VStack {
                     HStack {
                         stats
                     }
-                    .frame(height: topBarSize)
-                    .padding(.bottom, 15)
+                    .padding(.horizontal, 10)
 
                     GeometryReader { proxy in
-                        if pairedIndexes.count != options.existingCount {
-                            actions
-                        }
+                        actions
 
                         Color.white
                             .opacity(0.000001)
                             .gesture(makeDragGesture(size: proxy.size))
                     }
                 }
-                .onAppear {
-                    var fullLeftOptions = Array(options.questions.shuffled())
-                    if fullLeftOptions.count < 5 {
-                        for _ in fullLeftOptions.count..<5 {
-                            fullLeftOptions.append(emptyIdentifier)
-                        }
-                    }
-                    let fullRightOptions = fullLeftOptions.map {
-                        if $0 == emptyIdentifier {
-                            return emptyIdentifier
-                        } else {
-                            return options[$0]!
-                        }
-                    }
-                    leftOptions = Array(Array(fullLeftOptions)[0..<5])
-                    rightOptions = Array(fullRightOptions)[0..<5].shuffled()
-                }
+            } else {
+                endView
             }
         }
         .navigationTitle("Drag and Match")
-        .navigationBarTitleDisplayMode(.inline)
     }
 
     @ViewBuilder
@@ -100,31 +69,37 @@ struct DragAndMatchView: View {
                     Spacer()
                         .frame(width: proxy.size.width/5*3.5)
                     VStack {
-                        ForEach(Array(rightOptions.enumerated()), id: \.offset) { index, option in
-                            SingleFlipCardView(front: .constant(option),
-                                               back: .constant(""),
-                                               frontColor: .pink,
-                                               onFlip: { _ in .reject })
-                            .opacity(option == emptyIdentifier ? 0 : (
-                                pairedIndexes.contains(Array(options.answers).firstIndex(of: option)!) ? 0 : 1
-                            ))
+                        ForEach(rightArrangement, id: \.self) { optionIndex in
+                            if currentOptions.count == rightArrangement.count {
+                                SingleFlipCardView(front: .constant(currentOptions[optionIndex].answer),
+                                                   back: .constant(""),
+                                                   frontColor: .pink,
+                                                   onFlip: { _ in .reject })
+                                .opacity(currentOptions[optionIndex].answer == emptyIdentifier ? 0 : (
+                                    pairedQuestions.contains(currentOptions[optionIndex]) ? 0 : 1
+                                ))
+                            }
                         }
                     }
                     .offset(x: -10)
+                }.onAppear {
+                    generateNewOption()
                 }
                 // left options
                 HStack {
                     VStack {
-                        ForEach(Array(leftOptions.enumerated()), id: \.offset) { index, option in
-                            SingleFlipCardView(front: .constant(option),
-                                               back: .constant(""),
-                                               frontColor: .purple,
-                                               onFlip: { _ in .reject })
-                            .opacity(option == emptyIdentifier ? 0 : (
-                                pairedIndexes.contains(Array(options.questions).firstIndex(of: option)!) ? 0 : 1
-                            ))
-                            .offset(x: draggedCard == index ? translation.width : 0,
-                                    y: draggedCard == index ? translation.height : 0)
+                        ForEach(leftArrangement, id: \.self) { optionIndex in
+                            if currentOptions.count == leftArrangement.count {
+                                SingleFlipCardView(front: .constant(currentOptions[optionIndex].question),
+                                                   back: .constant(""),
+                                                   frontColor: .pink,
+                                                   onFlip: { _ in .reject })
+                                .opacity(currentOptions[optionIndex].question == emptyIdentifier ? 0 : (
+                                    pairedQuestions.contains(currentOptions[optionIndex]) ? 0 : 1
+                                ))
+                                .offset(x: draggedCard == currentOptions[optionIndex] ? translation.width : 0,
+                                        y: draggedCard == currentOptions[optionIndex] ? translation.height : 0)
+                            }
                         }
                     }
                     .offset(x: 10)
@@ -135,123 +110,47 @@ struct DragAndMatchView: View {
         }
     }
 
-    @ViewBuilder
-    var stats: some View {
-        Spacer()
-            .frame(width: 10)
-        ZStack {
-            Color.cyan
-                .frame(height: 50)
-                .cornerRadius(10)
-                .opacity(0.5)
-            HStack {
-                Text("Left")
-                    .padding(.bottom, 0)
-                Text("\(options.existingCount-pairedIndexes.count)")
-                    .font(.system(size: 30))
-            }
-        }
-
-        ZStack {
-            Color.green
-                .frame(height: 50)
-                .cornerRadius(10)
-                .opacity(0.5)
-            HStack {
-                Text("Matched")
-                    .padding(.bottom, 0)
-                Text("\(pairedIndexes.count)")
-                    .font(.system(size: 30))
-            }
-        }
-
-        ZStack {
-            Color.indigo
-                .frame(height: 50)
-                .cornerRadius(10)
-                .opacity(0.5)
-            HStack {
-                Text("Wrong")
-                    .padding(.bottom, 0)
-                Text("\(wrongAnswers)")
-                    .font(.system(size: 30))
-            }
-        }
-        Spacer()
-            .frame(width: 10)
-    }
-
     func makeDragGesture(size: CGSize) -> some Gesture {
         DragGesture()
             .onChanged { gesture in
+                // if the gesture started outside the left 1/3, ignore it
                 guard gesture.startLocation.x < size.width/3 else { return }
-                var clickedIndex = -1
-                // NOTE: This card height is hard coded to fit the iPhone 13 Pro. May not work
-                // with other models so may need reworking.
-                let cardHeight = size.height/5
-                for location in 0..<leftOptions.count {
-                    guard leftOptions[location] != emptyIdentifier else { continue }
-                    if !pairedIndexes.contains(Array(options.questions).firstIndex(of:
-                            leftOptions[location])!) &&
-                        gesture.startLocation.y > CGFloat(location) * cardHeight &&
-                        gesture.startLocation.y < CGFloat(location+1) * cardHeight {
-                        clickedIndex = location
-                    }
-                }
-                if clickedIndex != -1 {
-                    draggedCard = clickedIndex
-                    translation = gesture.translation
-                }
+
+                // update the translation
+                self.translation = gesture.translation
+
+                // figure out which card was dragged by the start location
+                let selectedIndex = Int(gesture.startLocation.y / (size.height/5))
+                self.draggedCard = currentOptions[selectedIndex]
             }
             .onEnded { gesture in
-                if gesture.startLocation.x < size.width/3 && gesture.location.x > size.width/3*2 {
-                    let cardHeight = size.height/5
 
-                    // get the index of the start card
-                    var startIndex = -1
-                    for location in 0..<leftOptions.count {
-                        guard leftOptions[location] != emptyIdentifier else { continue }
-                        if !pairedIndexes.contains(Array(options.questions).firstIndex(of:
-                                leftOptions[location])!) &&
-                            gesture.startLocation.y > CGFloat(location) * cardHeight &&
-                            gesture.startLocation.y < CGFloat(location+1) * cardHeight {
-                            startIndex = location
+                // only if the x coordinate is on the right 1/3 of the screen, calculate overlap
+                if gesture.location.x > size.width/3*2 {
+                    // figure out which card it was dragged over
+                    let selectedIndex = Int(gesture.location.y / (size.height/5))
+                    let overlapCard = currentOptions[rightArrangement[selectedIndex]]
+                    if overlapCard == draggedCard {
+                        // They answered correctly
+                        let existingScore = attempts[overlapCard] ?? (0, 0)
+                        attempts[overlapCard] = (existingScore.0, existingScore.1 + 1)
+                        withAnimation {
+                            pairedQuestions.append(overlapCard)
                         }
-                    }
-
-                    // get the index of the end card
-                    var endIndex = -1
-                    for location in 0..<leftOptions.count {
-                        if  gesture.location.y > CGFloat(location) * cardHeight &&
-                            gesture.location.y < CGFloat(location+1) * cardHeight {
-                            endIndex = location
-                        }
-                    }
-
-                    if startIndex != -1 && endIndex != -1 {
-                        let startName = leftOptions[startIndex]
-                        let endName = rightOptions[endIndex]
-                        if options[startName] == endName {
-                            pairedIndexes.append(Array(options.questions).firstIndex(of: startName)!)
-                            translation = .zero
-                            draggedCard = -1
-                            if let score = scores[options[startIndex]] as? Double, score > 0 {
-                                scores[options[startIndex]] = 1 - (score / (score+1))
-                            } else {
-                                scores[options[startIndex]] = 1
-                            }
-                            generateNewOption()
-                            return
-                        } else {
-                            wrongAnswers += 1
-                            // add 1 to the score. When the user finally gets it correct, this score
-                            // will be inverted, ending up with the percentage of times this was wrong.
-                            if let score = scores[options[startIndex]] as? Double {
-                                scores[options[startIndex]] = score + 1
-                            } else {
-                                scores[options[startIndex]] = 1
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                            self.draggedCard = nil
+                            self.translation = .zero
+                            withAnimation {
+                                generateNewOption()
                             }
                         }
+                        completed += 1
+                        return
+                    } else {
+                        // They answered incorrectly
+                        let existingScore = attempts[overlapCard] ?? (0, 0)
+                        attempts[overlapCard] = (existingScore.0 + 1, existingScore.1 + 1)
+                        wrong = (wrong ?? 0) + 1
                     }
                 }
 
@@ -259,44 +158,58 @@ struct DragAndMatchView: View {
                 withAnimation(.easeInOut(duration: 0.2)) {
                     translation = .zero
                 }
+
                 DispatchQueue.main.asyncAfter(deadline: .now() + 0.02, execute: {
-                    self.draggedCard = -1
+                    self.draggedCard = nil
                 })
             }
     }
 
     func generateNewOption() {
-        let leftovers = options.questions.enumerated().filter { index, string in
-            return !pairedIndexes.contains(index) && !leftOptions.contains(string)
-        }.map({ index, string in
-            return String(string)
-        })
-        for (index, option) in leftOptions.enumerated() {
-            let optionIndex = Array(options.questions).firstIndex(of: option)!
-            if pairedIndexes.contains(optionIndex) {
+        var leftovers = questions.filter { question in
+            !pairedQuestions.contains(question) && !currentOptions.contains(question)
+        }
+        for (index, option) in currentOptions.enumerated() {
+            if pairedQuestions.contains(option) {
                 guard !leftovers.isEmpty else { return }
 
                 // this index has just been matched, find a new one
                 if let newOption = leftovers.randomElement() {
-                    let rightOption = options[newOption]!
-                    let correspondingRightOptionIndex = rightOptions.firstIndex(of: options[option]!)!
+                    leftovers.removeAll(where: { $0 == newOption })
                     DispatchQueue.main.asyncAfter(deadline: .now() + 0.2, execute: {
                         withAnimation {
-                            self.leftOptions[index] = newOption
-                            self.rightOptions[correspondingRightOptionIndex] = rightOption
-                            self.rightOptions.shuffle()
+                            currentOptions[index] = newOption
+                            // shuffle the order of the answers
+                            rightArrangement = rightArrangement.shuffled()
                         }
                     })
                 }
+            }
+        }
+        while currentOptions.count < leftArrangement.count {
+            // this index has just been matched, find a new one
+            if let newOption = leftovers.randomElement() {
+                leftovers.removeAll(where: { $0 == newOption })
+                withAnimation {
+                    currentOptions.append(newOption)
+                    // shuffle the order of the answers
+                    rightArrangement = rightArrangement.shuffled()
+                }
+            } else {
+                currentOptions.append(.empty())
             }
         }
     }
 
     func restart() {
         generateNewOption()
-        pairedIndexes = []
-        let empty: [Double?] = options.map({ _ in nil })
-        self.scores = Dictionary(uniqueKeysWithValues: zip(options, empty))
+        pairedQuestions = []
+        attempts = [:]
+    }
+
+    @Environment(\.presentationMode) var presentationMode
+    func exit() {
+        presentationMode.wrappedValue.dismiss()
     }
 }
 
