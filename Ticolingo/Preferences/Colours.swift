f000e0fac777deb3abf60320d222bcb82a1cede5
Alyssa Aw Yong <alyssa.awyong@gmail.com>
2022-11-05 11:17:33 +0800

    Minor colour updates

--- Ticolingo/Preferences/Colours.swift
+++ Ticolingo/Preferences/Colours.swift
@@ -9,10 +9,16 @@ import Foundation
 import SwiftUI
 
 let primaryTextColour = Color(red: 246/255, green: 198/255, blue: 106/255)
-let secondaryTextColour = Color(red: 192/255, green: 100/255, blue: 171/255)
+let secondaryTextColour = Color(red: 235/255, green: 120/255, blue: 216/255)
 let tertiaryTextColour = Color(red: 194/255, green: 70/255, blue: 123/255)
 
 let backgroundColour = Color(red: 249/255, green: 241/255, blue: 251/255)
 
 let primaryFillerColour = Color(red: 170/255, green: 137/255, blue: 191/255)
 let secondaryFillerColour = Color(red: 202/255, green: 183/255, blue: 206/255)
+
+func setColours() {
+    UISearchBar.appearance().tintColor = UIColor.init(primaryTextColour)
+    UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.init(primaryTextColour)]
+    UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).backgroundColor = UIColor.init(primaryTextColour)
+}
