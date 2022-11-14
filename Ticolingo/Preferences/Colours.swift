07aed7718ddeb9a2119e5ea1dd0d65e1f0dce033
Elyssa <elyssajx4160@gmail.com>
2022-11-14 11:20:11 +0800

    Added like 7 colour customisation options which unfortunately does not sync

--- Ticolingo/Preferences/Colours.swift
+++ Ticolingo/Preferences/Colours.swift
@@ -8,14 +8,12 @@
 import Foundation
 import SwiftUI
 
-let primaryTextColour = Color(red: 246/255, green: 198/255, blue: 106/255)
-let secondaryTextColour = Color(red: 235/255, green: 120/255, blue: 216/255)
-let tertiaryTextColour = Color(red: 194/255, green: 70/255, blue: 123/255)
-
-let backgroundColour = Color(red: 249/255, green: 241/255, blue: 251/255)
-
-let primaryFillerColour = Color(red: 170/255, green: 137/255, blue: 191/255)
-let secondaryFillerColour = Color(red: 202/255, green: 183/255, blue: 206/255)
+var primaryTextColour = Color(red: 246/255, green: 198/255, blue: 106/255)
+var secondaryTextColour = Color(red: 120/255, green: 120/255, blue: 216/255)
+var tertiaryTextColour = Color(red: 194/255, green: 70/255, blue: 123/255)
+var backgroundColour = Color(red: 249/255, green: 241/255, blue: 251/255)
+var primaryFillerColour = Color(red: 170/255, green: 137/255, blue: 191/255)
+var secondaryFillerColour = Color(red: 202/255, green: 183/255, blue: 206/255)
 
 func setColours() {
     UISearchBar.appearance().tintColor = UIColor.init(primaryTextColour)
--- Ticolingo/Preferences/PreferencesView.swift
+++ Ticolingo/Preferences/PreferencesView.swift
@@ -19,14 +19,48 @@ struct PreferencesView: View {
 
     var body: some View {
         List {
-            Button(role: .destructive) {
-                showAlert = true
-                confirmProcessType = .resetVocab
-            } label: {
-                HStack {
-                    Image(systemName: "exclamationmark.triangle.fill")
-                    Text("Reset Vocabulary")
+            Section {
+                Button(role: .destructive) {
+                    showAlert = true
+                    confirmProcessType = .resetVocab
+                } label: {
+                    HStack {
+                        Image(systemName: "exclamationmark.triangle.fill")
+                        Text("Reset Vocabulary")
+                    }
                 }
+            }
+            Section("Color Scheme Picker") {
+                ColorPicker("Primary Text Colour \(primaryTextColour.description)", selection: .init(get: {
+                    primaryTextColour
+                }, set: {
+                    primaryTextColour = $0
+                }))
+                ColorPicker("Secondary Text Colour \(secondaryTextColour.description)", selection: .init(get: {
+                    secondaryTextColour
+                }, set: {
+                    secondaryTextColour = $0
+                }))
+                ColorPicker("Tertiary Text Colour \(tertiaryTextColour.description)", selection: .init(get: {
+                    tertiaryTextColour
+                }, set: {
+                    tertiaryTextColour = $0
+                }))
+                ColorPicker("Background Colour \(backgroundColour.description)", selection: .init(get: {
+                    backgroundColour
+                }, set: {
+                    backgroundColour = $0
+                }))
+                ColorPicker("Primary Filler Colour \(primaryFillerColour.description)", selection: .init(get: {
+                    primaryFillerColour
+                }, set: {
+                    primaryFillerColour = $0
+                }))
+                ColorPicker("Secondary Filler Colour \(secondaryFillerColour.description)", selection: .init(get: {
+                    secondaryFillerColour
+                }, set: {
+                    secondaryFillerColour = $0
+                }))
             }
         }
         .navigationTitle("Preferences")
