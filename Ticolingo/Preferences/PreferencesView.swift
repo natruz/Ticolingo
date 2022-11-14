32282c99dca8ccddfcbc9d09662508089711d137
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2022-11-14 15:49:38 +0800

    Make names more descriptive

--- Ticolingo/Preferences/PreferencesView.swift
+++ Ticolingo/Preferences/PreferencesView.swift
@@ -33,32 +33,32 @@ struct PreferencesView: View {
                 }
             }
             Section("Color Scheme Picker") {
-                ColorPicker("Primary Text Colour \n\(colors.primaryTextColour.description)", selection: .init(get: {
+                ColorPicker("Main Header Color", selection: .init(get: {
                     colors.primaryTextColour
                 }, set: {
                     colors.primaryTextColour = $0
                 }))
-                ColorPicker("Secondary Text Colour \n\(colors.secondaryTextColour.description)", selection: .init(get: {
+                ColorPicker("Section Header Color", selection: .init(get: {
                     colors.secondaryTextColour
                 }, set: {
                     colors.secondaryTextColour = $0
                 }))
-                ColorPicker("Tertiary Text Colour \n\(colors.tertiaryTextColour.description)", selection: .init(get: {
+                ColorPicker("Body Text Colour", selection: .init(get: {
                     colors.tertiaryTextColour
                 }, set: {
                     colors.tertiaryTextColour = $0
                 }))
-                ColorPicker("Background Colour \n\(colors.backgroundColour.description)", selection: .init(get: {
+                ColorPicker("Background Colour (UNUSED)", selection: .init(get: {
                     colors.backgroundColour
                 }, set: {
                     colors.backgroundColour = $0
                 }))
-                ColorPicker("Primary Filler Colour \n\(colors.primaryFillerColour.description)", selection: .init(get: {
+                ColorPicker("Primary Filler Colour (UNUSED)", selection: .init(get: {
                     colors.primaryFillerColour
                 }, set: {
                     colors.primaryFillerColour = $0
                 }))
-                ColorPicker("Secondary Filler Colour \n\(colors.secondaryFillerColour.description)", selection: .init(get: {
+                ColorPicker("List Background Color", selection: .init(get: {
                     colors.secondaryFillerColour
                 }, set: {
                     colors.secondaryFillerColour = $0
